2b52f71748387edfe74a6525c146ec807ca19e1d
DK\AEC <aec@sitecore.net>
2018-07-02 16:25:47 +0300

    Commit patch

---
+++ src/Sitecore.Support.227832/DefaultTicketManager.cs
@@ -0,0 +1,103 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Properties;
+using Sitecore.Diagnostics;
+using Sitecore.Pipelines.LoggedIn;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Support.Web.Authentication
+{
+  public class DefaultTicketManager : Sitecore.Web.Authentication.DefaultTicketManager
+  {
+
+
+    public DefaultTicketManager(BaseClient client) : base(client)
+    {
+    }
+    public override string CreateTicket(string userName, string startUrl)
+    {
+      Assert.ArgumentNotNull(userName, "userName");
+      return this.CreateTicket(userName, startUrl, true);
+    }
+    /// <summary>
+    /// Creates the ticket.
+    /// </summary>
+    /// <param name="userName">Name of the user.</param>
+    /// <param name="startUrl">The start URL.</param>
+    /// <param name="persist">If set to <c>true</c> [persist].</param>
+    /// <returns>
+    /// The ticket.
+    /// </returns>
+    public override string CreateTicket(string userName, string startUrl, bool persist)
+    {
+      Assert.ArgumentNotNullOrEmpty(userName, "userName");
+      Sitecore.Web.Authentication.Ticket ticket = GetExistingTicket(userName, true);
+      if (ticket == null)
+      {
+        Sitecore.Web.Authentication.Ticket existingTicket = GetExistingTicket(userName, false);
+        ticket = new Sitecore.Web.Authentication.Ticket
+        {
+          Id = ID.NewID.ToShortID().ToString(), //old value:((existingTicket != null) ? existingTicket.Id : null) ?? string.Empty) + ID.NewID.ToShortID().ToString()
+          UserName = userName,
+          Persist = persist
+        };
+      }
+      ticket.TimeStamp = DateTime.UtcNow;
+      if (!string.IsNullOrEmpty(startUrl))
+      {
+        this.CheckOnExternalUrl(startUrl);
+        ticket.StartUrl = startUrl;
+      }
+      ticket.Persist = persist;
+      if (!Settings.GetBoolSetting("Security.ShareAuthenticationTicket", false) && !string.IsNullOrEmpty(this.GetSessionId()))
+      {
+        ticket.ClientId = this.GetSessionId();
+      }
+      this.SaveTicket(ticket);
+      return ticket.Id;
+    }
+
+    /// <summary>
+    /// Gets the existing ticket.
+    /// </summary>
+    /// <param name="userName">Name of the user.</param>
+    /// <param name="checkShareAuthentication">checkShareAuthentication.</param>
+    /// <returns>
+    /// The existing ticket.
+    /// </returns>
+    private Sitecore.Web.Authentication.Ticket GetExistingTicket(string userName, bool checkShareAuthentication = true)
+    {
+      Assert.ArgumentNotNullOrEmpty(userName, "userName");
+      List<string> ticketKeyList = this.GetTicketKeyList();
+      string sessionId = this.GetSessionId();
+      if ((!Settings.GetBoolSetting("Security.ShareAuthenticationTicket", false) & checkShareAuthentication) && !string.IsNullOrEmpty(sessionId))
+      {
+        foreach (string current in ticketKeyList)
+        {
+          Sitecore.Web.Authentication.Ticket ticketByKey = base.GetTicketByKey(current);
+          if (ticketByKey != null && ticketByKey.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase) && ticketByKey.ClientId.Equals(sessionId, StringComparison.OrdinalIgnoreCase))
+          {
+            Sitecore.Web.Authentication.Ticket result = (!this.IsTicketExpired(ticketByKey, true)) ? ticketByKey : null;
+            return result;
+          }
+        }
+        return null;
+      }
+      foreach (string current2 in ticketKeyList)
+      {
+        Sitecore.Web.Authentication.Ticket ticketByKey2 = base.GetTicketByKey(current2);
+        if (ticketByKey2 != null && ticketByKey2.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase))
+        {
+          Sitecore.Web.Authentication.Ticket result = (!base.IsTicketExpired(ticketByKey2, true)) ? ticketByKey2 : null;
+          return result;
+        }
+      }
+      return null;
+    }
+  }
+}
--- src/Sitecore.Support.227832/Ticket.cs
+++ src/Sitecore.Support.227832/Ticket.cs
@@ -3,48 +3,19 @@ using Sitecore.Abstractions;
 using Sitecore.Configuration;
 using Sitecore.DependencyInjection;
 using Sitecore.Diagnostics;
+using Sitecore.Pipelines.LoggedIn;
 using System;
 using System.Web;
 
-namespace Sitecore.Pipelines.LoggedIn
+namespace Sitecore.Support.Pipelines.LoggedIn
 {
   /// <summary>
   /// Processor for storing a ticket.
   /// </summary>
-  public class Ticket : LoggedInProcessor
+  public class Ticket : Sitecore.Pipelines.LoggedIn.Ticket
   {
-    /// <summary>
-    /// Returns the ticket manager. Used to create a ticket.
-    /// </summary>
-    protected BaseTicketManager TicketManager
-    {
-      get;
-      private set;
-    }
 
-    /// <summary>
-    /// Returns default name of the ticket cookie.
-    /// </summary>
-    protected string TicketCookieName
-    {
-      get;
-      private set;
-    }
-
-    /// <summary>
-    /// Specifies the number of days before client "remember me" information expires.
-    /// </summary>
-    protected TimeSpan ClientPersistentLoginDuration
-    {
-      get;
-      private set;
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="T:Sitecore.Pipelines.LoggedIn.Ticket" /> class. Obsolete.
-    /// </summary>
-    [Obsolete("Please use another constructor instead.")]
-    public Ticket() : this(ServiceLocator.ServiceProvider.GetRequiredService<BaseTicketManager>())
+    public Ticket() : this(new Sitecore.Support.Web.Authentication.DefaultTicketManager(new Sitecore.DefaultClient()))
     {
     }
 
@@ -52,22 +23,8 @@ namespace Sitecore.Pipelines.LoggedIn
     /// Initializes a new instance of the <see cref="T:Sitecore.Pipelines.LoggedIn.Ticket" /> class.
     /// </summary>
     /// <param name="manager">TicketManager</param>
-    public Ticket(BaseTicketManager manager) : this(manager, "sitecore_userticket", Settings.Authentication.ClientPersistentLoginDuration)
+    public Ticket(Sitecore.Support.Web.Authentication.DefaultTicketManager manager) : base(manager, "sitecore_userticket", Settings.Authentication.ClientPersistentLoginDuration)
     {
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="T:Sitecore.Pipelines.LoggedIn.Ticket" /> class.
-    /// </summary>
-    /// <param name="manager"></param>
-    /// <param name="ticketCookieName"></param>
-    /// <param name="clientPersistentLoginDuration"></param>
-    protected Ticket(BaseTicketManager manager, string ticketCookieName, TimeSpan clientPersistentLoginDuration)
-    {
-      Assert.ArgumentNotNull(manager, "manager");
-      this.TicketManager = manager;
-      this.TicketCookieName = ticketCookieName;
-      this.ClientPersistentLoginDuration = clientPersistentLoginDuration;
     }
 
     /// <summary>
@@ -93,7 +50,7 @@ namespace Sitecore.Pipelines.LoggedIn
     /// <param name="args">The arguments.</param>
     /// <param name="ticketId">The ticket identifier.</param>
     /// <param name="context">The context.</param>
-    protected virtual void AppendTicketCookie(LoggedInArgs args, string ticketId, HttpContextBase context)
+    protected override void AppendTicketCookie(LoggedInArgs args, string ticketId, HttpContextBase context)
     {
       Assert.ArgumentNotNull(args, "args");
       Assert.ArgumentNotNull(context, "context");
